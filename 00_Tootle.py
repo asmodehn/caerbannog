9431d5cf104d0b9eb9dd8878dad577a6f191f6e7
AlexV <asmodehn@gmail.com>
2019-01-22 18:55:37 +0100

    first tootle

---
+++ 00_Tootle.py
@@ -0,0 +1,215 @@
+import math
+import turtle
+import cmd
+import enum
+import logging
+
+
+# Directly, naively, modeled in python 3.6
+# from the code shown in : https://www.youtube.com/watch?v=AG3KuqDbmhM
+
+# Side Note: Yes, Python has enums !
+class PenState(enum.Enum):
+    UP = -1
+    DOWN = 1
+
+
+# Code isolated from the side-effects (commands and display)
+# Made to look as close as possible from code presented in Scott Wlaschin's NDC talk
+# Additionally is serves us as a simulation without relying on any side effects.
+#
+# note that doing so we already, in a way, separated the "model" from the actual "implementation code".
+class SimTurtle:
+
+    currentPosition = (0, 0)
+    currentAngle = 0.0
+    currentPenState = PenState.DOWN
+    #: the list of lines drawn, as the expected side effect.
+    drawing = []
+
+    # no init, just like in the code model
+
+    def move(self, distance: int):
+        logging.info("simulating move {0}".format(distance))
+
+        endPosition = (self.currentPosition[0] + distance * math.cos(self.currentAngle), self.currentPosition[1] + distance * math.sin(self.currentAngle) )
+
+        if self.currentPenState == PenState.DOWN:
+            self.drawing.append((self.currentPosition, endPosition))
+
+        self.currentPosition = endPosition
+
+    def turn(self, angle: int):
+        logging.info("simulating turn {0} degree".format(angle))
+
+        newAngle = self.currentAngle + angle
+
+        self.currentAngle = newAngle
+
+    def penup(self):
+        logging.info("simulating pen UP")
+        self.currentPenState = PenState.UP
+
+    def pendown(self):
+        logging.info("simulating pen DOWN")
+        self.currentPenState = PenState.DOWN
+
+    # Adding these to complete the model with a minimum of useful functions
+    def home(self):
+        logging.info("simulating going home")
+        self.currentPosition = 0
+
+    def reset(self):
+        logging.info("simulating reset")
+        self.currentPosition = 0
+
+    # No bye, no deletion, python garbage collect...
+
+
+# The usual OO inheritance interface
+# taking turtle.Turtle as an unknown black box
+# => we do not know, so we simulate, based on hypothesis, and we experiment to confirm/infirm them.
+class InteractiveTurtle(turtle.Turtle):
+
+    model = SimTurtle()
+
+    # abasic comparison function to make sure our model stays on track...
+    def _compare_model(self):
+        assert self.model.currentPosition == self.position(), f"Model position {self.model.currentPosition} inconsistent with reality : {self.position()}"
+        assert self.model.currentAngle % 360 == self.heading(), f"Model angle {self.model.currentAngle} inconsistent with reality : {self.heading()}"
+
+    def move(self, distance: int):
+
+        # TMP HACK
+        distance = int(distance)
+
+        logging.info("move {0}".format(distance))
+        self._compare_model()
+
+        self.model.move(distance)
+        super().forward(distance=distance)
+
+        self._compare_model()
+
+    def right(self, angle: int):
+
+        # TMP HACK
+        angle = int(angle)
+
+        logging.info("turn {0} degree right".format(angle))
+        self._compare_model()
+
+        self.model.turn(angle * -1)
+        # Note here that with just a minor difference in interface, errors can creep in.
+        # This actually means a different model would have been a better fit for our actual implementation.
+        super().right(angle)
+
+        self._compare_model()
+
+    def left(self, angle: int):
+
+        # TMP HACK
+        angle = int(angle)
+
+        logging.info("turn {0} degree left".format(angle))
+        self._compare_model()
+
+        self.model.turn(angle)
+        super().left(angle)
+
+        self._compare_model()
+
+    def penup(self):
+        logging.info("pen UP")
+        # Not asserting : for later visual comparison only
+        self.model.penup()
+        super().penup()
+
+    def pendown(self):
+        logging.info("pen DOWN")
+        # Not asserting : for later visual comparison only
+        self.model.pendown()
+        super().pendown()
+
+    def home(self):
+        logging.info("going home")
+        self._compare_model()
+
+        self.model.home()
+        super().home()
+
+        self._compare_model()
+
+    def reset(self):
+        logging.info("RESET!")
+        self._compare_model()
+
+        self.model.reset()
+        super().reset()
+
+        self._compare_model()
+
+    def bye(self):
+        logging.info("BYE!")
+        super().getscreen().bye()
+
+
+# Usual cmd interface, OO-style
+class TurtleRepl(cmd.Cmd):
+
+    intro = 'Welcome to the tootle shell.   Type help or ? to list commands.\n'
+    prompt = '(tootle) '
+    tootle = InteractiveTurtle()
+
+    # ----- basic turtle commands -----
+    def do_move(self, distance):
+        'Move the turtle forward by the specified distance:  FORWARD 10'
+        self.tootle.move(distance)
+
+    def do_left(self, angle):
+        'Turn turtle right by given number of degrees:  LEFT 20'
+        self.tootle.left(angle)
+
+    def do_right(self, angle):
+        'Turn turtle right by given number of degrees:  RIGHT 20'
+        self.tootle.right(angle)
+
+    def do_home(self):
+        'Return turtle to the home postion:  HOME'
+        self.tootle.home()
+
+    # This is "read-only"
+    def do_position(self, arg):
+        'Print the current turle position:  POSITION'
+        print('Current position is {}'.format(self.tootle.position()))
+
+    # This is "read-only"
+    def do_heading(self, arg):
+        "Print the current turle heading in degrees:  HEADING"
+        print('Current heading is {}'.format(self.tootle.heading()))
+
+    # This is only cosmetic :-)
+    def do_color(self, arg):
+        'Set the color:  COLOR BLUE'
+        self.tootle.color(arg.lower())
+
+    def do_reset(self, arg):
+        'Clear the screen and return turtle to center:  RESET'
+        self.tootle.reset()
+
+    def do_bye(self, arg):
+        'Stop recording, close the turtle window, and exit:  BYE'
+        print('Thank you for using Turtle')
+        self.tootle.bye()
+        return True
+
+
+def parse(arg):
+    'Convert a series of zero or more numbers to an argument tuple'
+    return tuple(map(int, arg.split()))
+
+
+if __name__ == '__main__':
+    TurtleRepl().cmdloop()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # functional-python
-From Python to Functional Code, follow the white rabbit
+From Python to Functional Code, follow the white rabbit.
+
+In modern/latest Python (3.6), with a touch of interactivity...
+
+Inspired by the step by step introduction to functional programming by [Scott Wlaschin' NDC 2017 talk](https://www.youtube.com/watch?v=AG3KuqDbmhM)
+
+Along the way we are gathering and reusing useful code in a package on the side...
+
+- 00_Tootle : the Object Oriented (Interactive) Turtle. Using the usual inheritance-style interface from python classes.
+- 01_Pootle : A nicer, cleaner Pythonic Tootle
+
+
+
+#TODO :
+- [ ] somehow make a nice (interactive & visual) webpage of literal programming.
